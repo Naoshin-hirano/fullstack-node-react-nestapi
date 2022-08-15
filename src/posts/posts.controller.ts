0baaf0ecd8d7631fbf605146f2c6d5105c2a7dcd
Hirano <hnaoya1120@gmail.com>
2022-08-15 14:16:37 +0900

    DBなしでCRUD機能

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { PostModel } from './posts.model';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
@@ -6,7 +7,42 @@ export class PostsController {
     constructor(private readonly postsService: PostsService) { }
 
     @Get()
-    findAll() {
+    findAll(): PostModel[] {
         return this.postsService.findAll();
-    }
+    };
+
+    @Get(':id')
+    findById(@Param('id') id: string): PostModel {
+        return this.postsService.findById(id);
+    };
+
+    @Post()
+    create(
+        @Body('id') id: string,
+        @Body('title') title: string,
+        @Body('postText') postText: string,
+        @Body('username') username: string,
+    ): PostModel {
+        const post: PostModel = {
+            id,
+            title,
+            postText,
+            username,
+        };
+        return this.postsService.create(post);
+    };
+
+    @Patch(':id')
+    update(
+        @Param('id') id: string,
+        @Body('title') title: string,
+        @Body('postText') postText: string,
+    ): PostModel {
+        return this.postsService.update(id, title, postText);
+    };
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.postsService.delete(id);
+    };
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { PostModel } from './posts.model';
 
 @Injectable()
 export class PostsService {
+    private posts: PostModel[] = [];
 
-    findAll() {
-        return 'This is itemservice'
+    findAll(): PostModel[] {
+        return this.posts;
+    }
+
+    findById(id: string): PostModel {
+        return this.posts.find(post => post.id === id);
+    }
+
+    create(post: PostModel): PostModel {
+        this.posts.push(post);
+        return post;
+    }
+
+    update(id: string, title: string, postText: string): PostModel {
+        const post = this.findById(id);
+        post.title = title;
+        post.postText = postText;
+        return post;
+    }
+
+    delete(id: string): string {
+        this.posts = this.posts.filter(post => post.id !== id);
+        return '削除完了しました';
     }
 }
